f829b4e7e7814d6e89a2bb29e5d4484c81010101
mohit4 <mohitkumar2801@gmail.com>
2021-10-08 23:41:28 +0530

    Added messages support

--- app/saga/settings.py
+++ app/saga/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Parameters for defining Bootstrap message classes against each type of message constraints
+MESSAGE_TAGS = {
+    messages.DEBUG : 'alert-info',
+    messages.INFO : 'alert-info',
+    messages.SUCCESS : 'alert-success',
+    messages.WARNING : 'alert-warning',
+    messages.ERROR : 'alert-danger'
+}
