2dafa1c1a8b24757682db8a7858dbd498241eb01
mohit4 <mohitkumar2801@gmail.com>
2021-10-13 23:58:54 +0530

    Added Project and Character models

--- app/characterbuilder/models.py
+++ app/characterbuilder/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    """Contains information of a story character"""
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
